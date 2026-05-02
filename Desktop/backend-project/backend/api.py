001f27f4095aefeefbdb04fed7afdd1772dfd58d
daniyar-udel <daniyar@udel.edu>
2026-05-02 16:49:26 -0400

    Updates

--- Desktop/backend-project/backend/api.py
+++ Desktop/backend-project/backend/api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+country_dict = {
+    'Russia': ['Moscow', 'St. Petersburg', 'Novosibirsk', 'Ekaterinburg', 'Kazan'],
+    'USA': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia'],}
 
+from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get('/')
-async def main() -> dict:
-    return {'message': 'hello fastapi'}
+@app.get('/country/{country}')
+async def list_cities(country:str, limit:int) -> dict:
+    return {'country': country, 'cities': country_dict[country][0: limit]}
