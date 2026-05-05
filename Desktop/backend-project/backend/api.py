353528d6de670d679a796b22ed88de7d461d5dcf
daniyar-udel <daniyar@udel.edu>
2026-05-04 21:28:27 -0400

    Progress

--- Desktop/backend-project/backend/api.py
+++ Desktop/backend-project/backend/api.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
+
+profiles_dict = {
+    'alex': {'name': 'Александр', 'age': 33, 'phone': '+79463456789', 'email': 'alex@my-site.com'},
+}
 
 app = FastAPI()
 
-@app.get('/mobile/{model}')
-async def mobile(model: str) -> dict:
-    return {'model': model}
+@app.get('/users')
+async def retrieve_user_profile(username: Annotated[str | None,
+                                                   Query(min_length=2, max_length=50,
+                                                        description='Имя пользователя')] = None):
+    if username in profiles_dict:
+        return profiles_dict[username]
+    return {'message': f'Пользователь {username} не найден.'}
