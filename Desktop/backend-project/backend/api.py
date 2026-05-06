65258d61a202e63ab52e4efafa3284c49b646112
daniyar-udel <daniyar@udel.edu>
2026-05-05 22:46:55 -0400

    Progress

--- Desktop/backend-project/backend/api.py
+++ Desktop/backend-project/backend/api.py
@@ -1,16 +1,41 @@
-from fastapi import FastAPI, Query
-from typing import Annotated
-
-profiles_dict = {
-    'alex': {'name': 'Александр', 'age': 33, 'phone': '+79463456789', 'email': 'alex@my-site.com'},
-}
+from fastapi import FastAPI, status, HTTPException, Body
 
 app = FastAPI()
 
-@app.get('/users')
-async def retrieve_user_profile(username: Annotated[str | None,
-                                                   Query(min_length=2, max_length=50,
-                                                        description='Имя пользователя')] = None):
-    if username in profiles_dict:
-        return profiles_dict[username]
-    return {'message': f'Пользователь {username} не найден.'}
+car_db = {0: 'Lexus ct 200h'}
+
+@app.get('/cars')
+async def read_cars() -> dict:
+    return car_db
+
+@app.get('/cars/{cars_id}')
+async def read_cars_id(cars_id: int) -> str:
+    try:
+        return car_db[cars_id]
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, description='Car not found')
+    
+@app.post('/cars', status_code=status.HTTP_201_CREATED)
+async def create_car(car: str = Body()) -> str:
+    curr_ind = max(car_db) + 1 if car_db else 0
+    car_db[curr_ind] = car
+    return 'Car created'
+
+@app.put('/cars/{car_id}', status_code=status.HTTP_200_OK)
+async def update_car(car_id: int, car: str = Body()) -> str:
+    if car_id not in car_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Car not found')
+    car_db[car_id] = car
+    return 'Car updated'
+
+@app.delete('/cars/{car_id}', status_code=status.HTTP_200_OK)
+async def delete_car(car_id: int) -> str:
+    if car_id not in car_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Car not found')
+    car_db.pop(car_id)
+    return f'Car with {car_id} id deleted'
+
+@app.delete('/cars')
+async def delete_cars() -> str:
+    car_db.clear()
+    return 'Car data cleared'
