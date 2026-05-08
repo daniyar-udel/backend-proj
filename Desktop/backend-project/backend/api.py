0ca5896c8d93ce75bc150f9b00c3d11cb07e5935
daniyar-udel <daniyar@udel.edu>
2026-05-08 15:04:40 -0400

    Updates

--- Desktop/backend-project/backend/api.py
+++ Desktop/backend-project/backend/api.py
@@ -1,57 +1,53 @@
-from fastapi import FastAPI, status, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-class CreatPhone(BaseModel):
-    model: str
-    price: int
+class CreateBottle(BaseModel):
+    name: str = Field(max_length=15)
+    price: int = Field(ge=1)
 
-class Phones(BaseModel):
+class Bottle(CreateBottle):
     id: int
-    model: str
-    price: int
 
-phones_db: list[Phones] = [Phones(id=1, model='Galaxy A8', price=500)]
+bottles_db: list[Bottle] = [Bottle(id=1, name='Deer Park', price=1)]
 
-@app.get('/phones', response_model=list[Phones])
-async def read_phones() -> list[Phones]:
-    return phones_db
+@app.get('/bottles', response_model=list[Bottle])
+async def read_bottles() -> list[Bottle]:
+    return bottles_db
 
-@app.get('/phones/{phone_id}', response_model=Phones)
-async def read_phone(phone_id:int) -> Phones:
-    for phone in phones_db:
-        if phone.id == phone_id:
-            return phone
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='There is no phone')
+@app.get('/bottles/{bottle_id}', response_model=Bottle)
+async def read_bottle(bottle_id: int) -> Bottle:
+    for bottle in bottles_db:
+        if bottle.id == bottle_id:
+            return bottle
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Bottle not found')
 
-@app.post('/phones', response_model=Phones, status_code=status.HTTP_201_CREATED)
-async def create_phone(new_phone: CreatPhone) -> Phones:
-    curr_id = phones_db[-1].id + 1 if phones_db else 0
-    phone = Phones(id=curr_id, model=new_phone.model, price=new_phone.price)
-    phones_db.append(phone)
-    return phone
+@app.post('/bottles', response_model=Bottle)
+async def create_bottle(new_bottle: CreateBottle) -> Bottle:
+    next_id = len(bottles_db) + 1 if bottles_db else 0
+    bottle = Bottle(id=next_id, name=new_bottle.name, price=new_bottle.price)
+    bottles_db.append(bottle)
+    return bottle
 
-@app.put('/phones/{phone_id}', response_model=Phones, status_code=status.HTTP_200_OK)
-async def change_phone(phone_id:int, new_phone: CreatPhone) -> Phones:
-    for i, phone in enumerate(phones_db):
-        if phone.id == phone_id:
-            phone = Phones(id=phone_id, model=new_phone.model, price=new_phone.price)
-            phones_db[i] = phone
-            return phone
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='There is no phone')
-        
-@app.delete('/phones/{phone_id}', response_model=Phones, status_code=status.HTTP_200_OK)
-async def delete_phone(phone_id: int) -> Phones:
-    for i, phone in enumerate(phones_db):
-        if phone.id == phone_id:
-            phones_db.pop(i)
-            return phone
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='There is no phone')
+@app.put('/bottles/{bottle_id}', response_model=Bottle)
+async def update_bottle(bottle_id:int, new_bottle: CreateBottle) -> Bottle:
+    for i, bottle in enumerate(bottles_db):
+        if bottle.id == bottle_id:
+            update_bottle = Bottle(id=bottle_id, name=new_bottle.name, price=new_bottle.price)
+            bottles_db[i] = update_bottle
+            return update_bottle
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Bottle not found')
 
-@app.delete('/phones', response_model=list[Phones])
-async def delete_phones() -> list[Phones]:
-    phones_db.clear()
-    return phones_db
+@app.delete('/bottles/{bottle_id}', response_model=Bottle)
+async def delete_bottle(bottle_id:int) -> Bottle:
+    for i, bottle in enumerate(bottles_db):
+        if bottle.id == bottle_id:
+            bottles_db.pop(i)
+            return bottle
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Bottle not found')
 
-    
+@app.delete('/bottles', response_model=list[Bottle])
+async def delete_bottles() -> list[Bottle]:
+    bottles_db.clear()
+    return bottles_db
